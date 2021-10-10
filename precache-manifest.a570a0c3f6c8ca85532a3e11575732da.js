2d870a2f028cdf9c802ed53b3a936dc23fd8376b
Jaime Cabrera <jycabrerap@unl.edu.ec>
2021-10-10 13:30:07 -0500

    Updates

---
+++ precache-manifest.a570a0c3f6c8ca85532a3e11575732da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d068c6a611e627b127971974b838488d",
+    "url": "/gift-expert/index.html"
+  },
+  {
+    "revision": "fce734e5dadcb7c37d93",
+    "url": "/gift-expert/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "c76fc7e01808073d3a7c",
+    "url": "/gift-expert/static/css/main.db933e7a.chunk.css"
+  },
+  {
+    "revision": "fce734e5dadcb7c37d93",
+    "url": "/gift-expert/static/js/2.05c647ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gift-expert/static/js/2.05c647ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76fc7e01808073d3a7c",
+    "url": "/gift-expert/static/js/main.4445a19a.chunk.js"
+  },
+  {
+    "revision": "04b32d18bef1d8a254a3",
+    "url": "/gift-expert/static/js/runtime-main.badb6182.js"
+  }
+]);
